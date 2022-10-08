94b1de132bd3895391fdc6295e2eb9b3654618fb
Arun Jangra <arunjngra89@gmail.com>
2022-10-08 18:33:54 +0530

    commit

--- src/Components/Domains.jsx
+++ src/Components/Domains.jsx
@@ -1,64 +1,79 @@
 import React from "react";
 import gsap from "gsap";
-import {ScrollTrigger} from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-function Domains(){
-    const domains = [
+function Domains() {
+  const domains = [
+    {
+      heading: "Technical",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita accusantium placeat repellat laborum, eaque temporibus sed ea quas explicabo, suscipit provident officiis praesentium! Molestiae quis id impedit, maxime consequuntur, delectus nam enim incidunt doloremque quod, commodi sit ut beatae dolorum obcaecati voluptate vel? Asperiores adipisci veritatis itaque doloribus aliquid sequi.",
+      subDomains: ["AI/ML", "Blockchain", "Web Dev"],
+    },
+    {
+      heading: "Creatives",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita accusantium placeat repellat laborum, eaque temporibus sed ea quas explicabo, suscipit provident officiis praesentium! Molestiae quis id impedit, maxime consequuntur, delectus nam enim incidunt doloremque quod, commodi sit ut beatae dolorum obcaecati voluptate vel? Asperiores adipisci veritatis itaque doloribus aliquid sequi.",
+      subDomains: ["Graphic Designing", "VFX"],
+    },
+    {
+      heading: "Corporate",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita accusantium placeat repellat laborum, eaque temporibus sed ea quas explicabo, suscipit provident officiis praesentium! Molestiae quis id impedit, maxime consequuntur, delectus nam enim incidunt doloremque quod, commodi sit ut beatae dolorum obcaecati voluptate vel? Asperiores adipisci veritatis itaque doloribus aliquid sequi.",
+      subDomains: [
+        "Content",
+        "Sponsorship",
+        "Event Management",
+        "Public Relations",
+      ],
+    },
+  ];
+  let domainsRef = React.useRef(null);
+  React.useEffect(() => {
+    gsap.context(() => {
+      gsap.fromTo(
+        ".heading",
         {
-            heading: "Technical",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita accusantium placeat repellat laborum, eaque temporibus sed ea quas explicabo, suscipit provident officiis praesentium! Molestiae quis id impedit, maxime consequuntur, delectus nam enim incidunt doloremque quod, commodi sit ut beatae dolorum obcaecati voluptate vel? Asperiores adipisci veritatis itaque doloribus aliquid sequi.",
-            subDomains: ["AI/ML", "Blockchain", "Web Dev"]
+          opacity: 0,
+          y: 20,
+          scrollTrigger: {
+            trigger: ".heading",
+            toggleAttribute: "restart none none none",
+          },
         },
         {
-            heading: "Creatives",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita accusantium placeat repellat laborum, eaque temporibus sed ea quas explicabo, suscipit provident officiis praesentium! Molestiae quis id impedit, maxime consequuntur, delectus nam enim incidunt doloremque quod, commodi sit ut beatae dolorum obcaecati voluptate vel? Asperiores adipisci veritatis itaque doloribus aliquid sequi.",
-            subDomains: ["Graphic Designing", "UI/UX", "Video Editing"]
-        },
-        {
-            heading: "Corporate",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita accusantium placeat repellat laborum, eaque temporibus sed ea quas explicabo, suscipit provident officiis praesentium! Molestiae quis id impedit, maxime consequuntur, delectus nam enim incidunt doloremque quod, commodi sit ut beatae dolorum obcaecati voluptate vel? Asperiores adipisci veritatis itaque doloribus aliquid sequi.",
-            subDomains: ["Content", "Sponsorship", "Event Management", "Public Relations"]
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          scrollTrigger: {
+            trigger: ".heading",
+            toggleAttribute: "restart none none none",
+          },
         }
-    ];
-    let domainsRef = React.useRef(null);
-    React.useEffect(()=>{
-        gsap.context(()=>{
-            gsap.fromTo(".heading", 
-            {
-                opacity: 0, 
-                y: 20, 
-                scrollTrigger: {
-                    trigger: ".heading",
-                    toggleAttribute: "restart none none none"
-                }
-            }, 
-            {
-                opacity: 1, 
-                y: 0, 
-                duration: 1,
-                scrollTrigger: {
-                    trigger: ".heading",
-                    toggleAttribute: "restart none none none"
-                }
-            });
-            gsap.from(".domain", {opacity: 0, x: -20, });
-            gsap.to(".domain", {opacity: 1, x: 0, delay: 0.2 });
-        }, domainsRef);
-    }, []);
+      );
+      gsap.from(".domain", { opacity: 0, x: -20 });
+      gsap.to(".domain", { opacity: 1, x: 0, delay: 0.2 });
+    }, domainsRef);
+  }, []);
 
-    return (
-        <div className="domains" id="domains" ref={el => domainsRef = el}>
-            <h1 className="heading">Domains</h1>
-            {domains.map((domain) => {
-                return (
-                    <section className="domain">
-                        <h2>{domain.heading}</h2>
-                        <p>{domain.description}</p>
-                        <ul>{domain.subDomains.map((subDomain)=> <li>{subDomain}</li>)}</ul>
-                    </section>
-                )
-            })};
-        </div>
-    )
+  return (
+    <div className="domains" id="domains" ref={(el) => (domainsRef = el)}>
+      <h1 className="heading">Domains</h1>
+      {domains.map((domain) => {
+        return (
+          <section className="domain">
+            <h2>{domain.heading}</h2>
+            <p>{domain.description}</p>
+            <ul>
+              {domain.subDomains.map((subDomain) => (
+                <li>{subDomain}</li>
+              ))}
+            </ul>
+          </section>
+        );
+      })}
+      ;
+    </div>
+  );
 }
 export default Domains;
