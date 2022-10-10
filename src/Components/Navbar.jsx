dc8518d0e279cb11a643542b5d4ef1e576ef43fb
Arun Jangra <arunjngra89@gmail.com>
2022-10-10 21:06:49 +0530

    fix : font-fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,34 +5,46 @@ import gsap from "gsap";
 function Navbar() {
   let navRef = React.useRef(null);
   let tl = React.useRef(null);
-  React.useEffect(()=>{
-    gsap.context(()=>{
+  React.useEffect(() => {
+    gsap.context(() => {
       tl.current = gsap
-      .timeline()
-      .fromTo(navRef, {opacity: 0, yPercent: 100}, {opacity: 1, yPercent: 0, duration: 1, delay: .5})
-      .fromTo(navRef, {css: {justifyContent: "center", background: "none"}}, {
-         css: {
-           justifyContent: "space-between",
-           background: "#232323",
-         },
-         scrollTrigger: {
-           trigger: navRef,
-           start: "center top",
-           toggleActions: "play none reverse none",
-         }
-      })
-      .fromTo(".logo", {css:{display: "none"}}, {
-       css:{display: "block"},
-       scrollTrigger: {
-         trigger: navRef,
-         start: "center top",
-         toggleActions: "play none reverse reverse",
-      }
-      })
-    })
-  })
+        .timeline()
+        .fromTo(
+          navRef,
+          { opacity: 0, yPercent: 100 },
+          { opacity: 1, yPercent: 0, duration: 1, delay: 0.5 }
+        )
+        .fromTo(
+          navRef,
+          { css: { justifyContent: "center", background: "none" } },
+          {
+            css: {
+              justifyContent: "space-between",
+              background: "#232323",
+            },
+            scrollTrigger: {
+              trigger: navRef,
+              start: "center top",
+              toggleActions: "play none reverse none",
+            },
+          }
+        )
+        .fromTo(
+          ".logo",
+          { css: { display: "none" } },
+          {
+            css: { display: "block" },
+            scrollTrigger: {
+              trigger: navRef,
+              start: "center top",
+              toggleActions: "play none reverse reverse",
+            },
+          }
+        );
+    });
+  });
   return (
-    <nav ref={el => navRef=el}>
+    <nav ref={(el) => (navRef = el)}>
       <a href="https://www.dscommunity.in" className="logo">
         <img src={Logo} alt="Data Science Community" />
       </a>
@@ -50,7 +62,7 @@ function Navbar() {
           <PopupButton
             id="gSkbTCDo"
             style={{ fontSize: 15 }}
-            className="my-button nav-typform-btn"
+            className="my-button nav-typform-btn nav-btn"
           >
             Apply 🚀
           </PopupButton>
