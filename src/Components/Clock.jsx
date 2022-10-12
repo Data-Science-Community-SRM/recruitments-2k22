c54e7420cacacdb70485e61aefb3082e35660fd1
Arun Jangra <arunjngra89@gmail.com>
2022-10-12 14:02:22 +0530

    fix : date

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,40 +1,40 @@
-import React, {Fragment} from "react";
-function Clock({days, hours, minutes, seconds}){
-    return (
-        <Fragment>
-            <section className="timer-container content">
-                <p>Launch In</p>
-                <section className="timer">
-                    <div className="clock">
-                        <section>
-                            <p>{days}</p>
-                            <small>D</small>
-                        </section>
-                        <span>:</span>
-                        <section>
-                            <p>{hours}</p>
-                            <small>H</small>
-                        </section>
-                        <span>:</span>
-                        <section>
-                            <p>{minutes}</p>
-                            <small>M</small>
-                        </section>
-                        <span>:</span>
-                        <section>
-                            <p>{seconds}</p>
-                            <small>S</small>
-                        </section>
-                    </div>
-                </section>
+import React, { Fragment } from "react";
+function Clock({ days, hours, minutes, seconds }) {
+  return (
+    <Fragment>
+      <section className="timer-container content">
+        <p>Launch In</p>
+        <section className="timer">
+          <div className="clock">
+            <section>
+              <p>{days}</p>
+              <small>D</small>
             </section>
-        </Fragment>
-    );
-};
-Clock.defaultProps={
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
+            <span>:</span>
+            <section>
+              <p>{hours}</p>
+              <small>H</small>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{minutes}</p>
+              <small>M</small>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{seconds}</p>
+              <small>S</small>
+            </section>
+          </div>
+        </section>
+      </section>
+    </Fragment>
+  );
 }
+Clock.defaultProps = {
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+};
 export default Clock;
